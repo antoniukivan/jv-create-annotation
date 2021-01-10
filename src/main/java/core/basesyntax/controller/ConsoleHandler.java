c97bbc6452ef961bf049245ed0959ee4aabd4895
antoniukivan <ivan.antonuk1992@gmail.com>
2021-01-10 21:31:30 +0200

    add lib package and annotation @Inject

--- src/main/java/core/basesyntax/controller/ConsoleHandler.java
+++ src/main/java/core/basesyntax/controller/ConsoleHandler.java
@@ -2,10 +2,12 @@ package core.basesyntax.controller;
 
 import core.basesyntax.dao.BetDao;
 import core.basesyntax.dao.BetDaoImpl;
+import core.basesyntax.lib.Inject;
 import core.basesyntax.model.Bet;
 import java.util.Scanner;
 
 public class ConsoleHandler {
+    @Inject
     private final BetDao betDao = new BetDaoImpl();
 
     public void handle() {
