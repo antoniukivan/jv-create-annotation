19498af52b0d3215f2e8269828bb1f2d9c6dd42c
antoniukivan <ivan.antonuk1992@gmail.com>
2021-01-10 23:37:42 +0200

    refactor method getInstance(Class)

--- src/main/java/core/basesyntax/lib/Injector.java
+++ src/main/java/core/basesyntax/lib/Injector.java
@@ -3,6 +3,7 @@ package core.basesyntax.lib;
 import core.basesyntax.dao.BetDao;
 import core.basesyntax.dao.UserDao;
 import core.basesyntax.factory.BetDaoImplFactory;
+import core.basesyntax.factory.UserDaoImplFactory;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -17,11 +18,10 @@ public class Injector {
             if (field.getAnnotation(Inject.class) != null
                     && field.getType().isAnnotationPresent(Dao.class)) {
                 field.setAccessible(true);
-                switch (field.getType()) {
-                    case BetDao.class:
-                        field.set(instance, BetDaoImplFactory.getBetDao());
-                    case UserDao.class:
-                        field.set(instance, UserDa);
+                if (BetDao.class.equals(field.getType())) {
+                    field.set(instance, BetDaoImplFactory.getBetDao());
+                } else if (UserDao.class.equals(field.getType())) {
+                    field.set(instance, UserDaoImplFactory.getUserDao());
                 }
             }
         }
