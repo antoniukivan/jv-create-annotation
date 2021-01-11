090af97e15b62fecdb8aec2b7b9b89fee6f95b2d
antoniukivan <ivan.antonuk1992@gmail.com>
2021-01-11 12:20:03 +0200

    refactor getInstance(Class) method

--- src/main/java/core/basesyntax/lib/Injector.java
+++ src/main/java/core/basesyntax/lib/Injector.java
@@ -9,22 +9,21 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 
 public class Injector {
-    public static Object getInstance(Class clazz) throws NoSuchMethodException,
+    public static Object getInstance(Class<?> clazz) throws NoSuchMethodException,
             IllegalAccessException, InvocationTargetException, InstantiationException {
         Object instance = clazz.getDeclaredConstructor().newInstance();
         Field[] declaredFields = clazz.getDeclaredFields();
         for (Field field : declaredFields) {
-            if (field.getAnnotation(Inject.class) != null) {
-                if (field.getType().isAnnotationPresent(Dao.class)) {
-                    field.setAccessible(true);
-                    if (BetDao.class.equals(field.getType())) {
-                        field.set(instance, BetDaoImplFactory.getBetDao());
-                    } else if (UserDao.class.equals(field.getType())) {
-                        field.set(instance, UserDaoImplFactory.getUserDao());
-                    }
-                } else {
-                    throw new NoSuchDaoFoundException("Invalid type: " + field.getType());
+            if (field.getAnnotation(Inject.class) != null
+                    && field.getType().isAnnotationPresent(Dao.class)) {
+                field.setAccessible(true);
+                if (BetDao.class.equals(field.getType())) {
+                    field.set(instance, BetDaoImplFactory.getBetDao());
+                } else if (UserDao.class.equals(field.getType())) {
+                    field.set(instance, UserDaoImplFactory.getUserDao());
                 }
+            } else {
+                throw new NoSuchDaoFoundException("Invalid type: " + field.getType());
             }
         }
         return instance;
